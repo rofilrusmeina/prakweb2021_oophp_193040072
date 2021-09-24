0ac4328513c9bd6061edba006cf6b7f250e344fb
rofilrusmeina <58686996+rofilrusmeina@users.noreply.github.com>
2021-09-25 01:05:28 +0700

    Add files via upload
    static

---
+++ prakweb2021_oophp_193040072/static.php
@@ -0,0 +1,36 @@
+<?php 
+
+// class contohstatic {
+//    public static $angka = 1;
+
+ //   public static function halo() {
+ //       return "Halo " . self::$angka . " kali.";
+  //  }
+//}
+
+//echo contohstatic::$angka;
+//echo "<br>";
+//echo contohstatic::halo();
+//echo "<hr>";
+//echo contohstatic::halo();
+
+class contoh {
+    public static $angka = 1;
+
+    public function halo() {
+        return "Halo " . self::$angka++ . " kali. <br>";
+    }
+}
+
+$obj = new contoh;
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
+
+echo "<hr>";
+
+$obj2 = new contoh;
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
+?>
