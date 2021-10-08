3d92e89dc72217f38466eb1c499d93b03227a6c0
rofilrusmeina <58686996+rofilrusmeina@users.noreply.github.com>
2021-10-08 21:14:22 +0700

    Add files via upload
    Assets tugas 4

---
+++ prakweb2021_oophp_193040072/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container"></div>
+        <h1 class="mt-4">About Me</h1>
+        <img src="<?= BASEURL; ?>" alt="Rofil">
+        <p>Halo, nama saya <?= $data['nama'];?> umur saya <?= $data['umur']; ?> tahun saya sebagai <?= $data['pekerjaan']; ?></p>
+</div>
